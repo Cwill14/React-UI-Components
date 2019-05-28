39491993d5ebe1219dba244171ddb942bc881cae
Clark Williams <theclarkwilliams@gmail.com>
2019-05-28 16:04:10 -0600

    MVP for project 1

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,8 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <div className="card-banner"></div>
+        <img src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
+
     );
 };
 
